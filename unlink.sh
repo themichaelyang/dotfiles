22a6c094ec8719ee0b30a1cd9426d7402396180e
Michael Yang <themichaelyang@gmail.com>
2017-08-22 15:51:51 -0700

    added unlinking script

---
+++ unlink.sh
@@ -0,0 +1,8 @@
+echo "WARNING: This script will delete all symlinked dotfiles in ~."
+read -p "Proceed? (y/n): " yn
+case $yn in
+    [Yy]* ) echo "Deleting: " \
+	    && find $HOME -maxdepth 1 \( -type l -and -name ".*" \) -print -delete \
+            && echo "Finished deleting.";;
+    * ) echo "Cancelled.";;
+esac
