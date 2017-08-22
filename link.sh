1c97a389e384b85c92f57003da3b90cc149eb7a3
Michael Yang <themichaelyang@gmail.com>
2017-08-22 16:17:46 -0700

    fixed vimrc syntax highlighting

--- link.sh
+++ link.sh
@@ -10,7 +10,7 @@ function link {
     local target=$HOMEDIR$2    
 
     if ! [[ -e "$target" ]]; then
-	      ln -s $origin $target && echo -e "\033[0;32mADDED:\033[0m $target."
+        ln -s $origin $target && echo -e "\033[0;32mADDED:\033[0m $target."
     else 
         echo -e "\033[0;31mERROR:\033[0m Target $target already exists"
     fi 
