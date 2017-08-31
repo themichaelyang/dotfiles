d0649833d30487d698a12b90ec38a837532ff9ab
Michael Yang <themichaelyang@gmail.com>
2017-08-31 12:04:52 -0700

    added global gitignore

--- link.sh
+++ link.sh
@@ -7,14 +7,19 @@ HOMEDIR=$HOME/
 
 function link {
     local origin=$BASEDIR$1
-    local target=$HOMEDIR$2    
+    local target=$HOMEDIR$2
 
     if ! [[ -e "$target" ]]; then
         ln -s $origin $target && echo -e "\033[0;32mADDED:\033[0m $target."
-    else 
+    else
         echo -e "\033[0;31mERROR:\033[0m Target $target already exists"
-    fi 
+    fi
 }
+
+# Remember, if you want to change the filename of any these,
+# update their references in .bashrc, and .gitconfig
+
+echo "Setting up symlinks..."
 
 # Bash!
 link shell/bashrc .bashrc
@@ -31,6 +36,9 @@ link vim/vimrc .vimrc
 # Git
 link git/gitconfig .gitconfig
 link git/gitprivate .gitprivate
+link git/gitignore-global .gitignore-global
 
-echo "Finished creating symlinks. Don't forget to restart your terminal."
+echo "Sourcing bashrc..."
+source ~/.bashrc
 
+echo "Finished creating symlinks."
