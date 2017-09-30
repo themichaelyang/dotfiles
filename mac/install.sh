b020f5247013017e46c04855ab8086ff504b6297
Michael Yang <themichaelyang@gmail.com>
2017-09-30 01:53:52 -0400

    added pandoc and basictex to mac install

--- mac/install.sh
+++ mac/install.sh
@@ -8,6 +8,8 @@ echo "Installing homebrew and mac defaults..."
 
 echo "Installing homebrew apps and packages..."
 brew bundle
+# Install basictex from file (homebrew doesn't provide an easily pipeable pkg output, hence the mess")
+open $(brew cask info basictex | sed -n 3p | cut -d " " -f1)/$(brew cask info basictex | sed -n 8p | cut -d " " -f1)
 
 echo "Starting f.lux and spectacle..."
 open -a spectacle
