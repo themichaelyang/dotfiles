fc09115e091e726284b98dc4d91f995a7aba1987
Michael Yang <themichaelyang@gmail.com>
2018-06-22 13:09:45 -0400

    fixed git tab completion

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Use an older version because
+# https://apple.stackexchange.com/questions/327817/git-completion-bash-producing-error-on-macos-sierra-10-12-6
+LINK="https://raw.githubusercontent.com/git/git/v2.17.1/contrib/completion/git-completion.bash"
 
 # Install Git Tab Completion script
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $BASEDIR/git-completion
+curl $LINK -o $BASEDIR/git-completion
