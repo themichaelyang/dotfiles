d81613f9093fa9002c148773eb564cab8e414c9a
Michael Yang <michaelyang@MacBook-Pro.home>
2018-09-14 17:02:53 -0400

    fixed mac setup install script

--- mac/install.sh
+++ mac/install.sh
@@ -3,13 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing homebrew and mac defaults..."
-./${DIR}/brew.sh
-./${DIR}/mac.sh
+. ${DIR}/brew.sh
+. ${DIR}/mac.sh
 
 echo "Installing homebrew apps and packages..."
 brew bundle
 # Install basictex from file (homebrew doesn't provide an easily pipeable pkg output, hence the mess")
-open $(brew cask info basictex | sed -n 3p | cut -d " " -f1)/$(brew cask info basictex | sed -n 8p | cut -d " " -f1)
+# open $(brew cask info basictex | sed -n 3p | cut -d " " -f1)/$(brew cask info basictex | sed -n 8p | cut -d " " -f1)
 
 echo "Starting f.lux and spectacle..."
 open -a spectacle
