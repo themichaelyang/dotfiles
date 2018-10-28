e7c29ca6815495ea68764d0567c4b613c47b5106
Michael Yang <themichaelyang@gmail.com>
2018-10-28 16:15:38 -0700

    removed jupyter theme selector

--- notebook/install-theme-selector.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p $(jupyter --data-dir)/nbextensions
-cd $(jupyter --data-dir)/nbextensions
-mkdir jupyter_themes && cd jupyter_themes
-wget https://raw.githubusercontent.com/merqurio/jupyter_themes/master/theme_selector.js
-# Activate the extension
-cd ../ && jupyter nbextension enable jupyter_themes/theme_selector
