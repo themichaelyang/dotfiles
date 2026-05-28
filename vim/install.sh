14f3a826e6e7607faa438dbaeb481d5d81f43ca7
Michael Yang <themichaelyang@gmail.com>
2026-05-28 14:33:41 -0700

    update vim config use vim plug subtree

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # install vim-plug
-echo "Installing vim-plug manager..."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# echo "Installing vim-plug manager..."
+# cp ~/dotfiles/vim/plugins/vim-plug/plug.vim ~/.vim/autoload/plug.vim
 
 echo "Installing vim plugins..."
 vim +PlugUpgrade +qall
