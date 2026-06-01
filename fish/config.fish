af531f02b3754b22a91c96e86453c76b319984d7
Michael Yang <themichaelyang@gmail.com>
2026-06-01 01:16:58 -0700

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+alias list 'ls -lA'
+
+alias up 'cd ..'
+alias back 'cd -'
+alias down 'cd (git rev-parse --show-toplevel)'
+
+alias dotfiles 'cd ~/dotfiles'
+
+alias rm 'echo "rm is disabled! Use trash or remove."'
+alias remove '/bin/rm -irv'
+alias cp 'cp -i'
+alias mv 'mv -i'
