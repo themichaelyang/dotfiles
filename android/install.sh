9d163f5746f26198cbd1484dfb02f24369c5e57a
Michael Yang <themichaelyang@gmail.com>
2026-06-04 01:36:38 -0700

    add claude code install for android

---
+++ android/install.sh
@@ -0,0 +1,6 @@
+# most recent claude code using node cli instead of native binary
+# since no native build for android
+pkg install nodejs
+npm install -g @anthropic-ai/claude-code@2.1.94
+
+pkg install helix
